61c6dcf3aece7d603daab7d078c3a1db59038247
Vaughn Scott <44189993+eyeoverthink@users.noreply.github.com>
2026-09-05 04:06:07 -0700

    FOUNDATION-001: implement renderer-independent Transform

--- fraymus/core/Transform.java
+++ fraymus/core/Transform.java
@@ -1,12 +1,25 @@
 package fraymus.core;
 
-public class Transform implements AutoCloseable {
-    public Transform() {
-        System.out.println("Entered fraymus.core.Transform");
-    }
+/** Renderer-independent spatial state. No OpenGL/LWJGL dependencies. */
+public final class Transform {
+    private double x;
+    private double y;
+    private double rotation;
+    private double scaleX = 1.0;
+    private double scaleY = 1.0;
 
-    @Override
-    public void close() {
-        System.out.println("Exited fraymus.core.Transform");
-    }
+    public Transform() {}
+
+    public Transform(double x, double y) { this.x = x; this.y = y; }
+
+    public double getX() { return x; }
+    public double getY() { return y; }
+    public double getRotation() { return rotation; }
+    public double getScaleX() { return scaleX; }
+    public double getScaleY() { return scaleY; }
+
+    public Transform setPosition(double x, double y) { this.x = x; this.y = y; return this; }
+    public Transform translate(double dx, double dy) { x += dx; y += dy; return this; }
+    public Transform setRotation(double rotation) { this.rotation = rotation; return this; }
+    public Transform setScale(double x, double y) { scaleX = x; scaleY = y; return this; }
 }
